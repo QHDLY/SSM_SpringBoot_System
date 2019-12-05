7e717813679544863513991edc40c701ce522b86
QHDLY <987941243@qq.com>
2019-12-05 19:48:38 +0800

    SSM整合

--- 15-SpringBoot-data-jpa/src/main/java/cn/liuy/dao/StudentJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.liuy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.liuy.pojo.Student;
-
-public interface StudentJpaRepository extends JpaRepository<Student, Integer > {
-
-}
--- 15-SpringBoot-data-jpa/src/main/java/cn/liuy/dao/UserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.liuy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.liuy.pojo.Subordinate;
-
-public interface UserJpaRepository extends JpaRepository<Subordinate, Integer> {
-
-}
