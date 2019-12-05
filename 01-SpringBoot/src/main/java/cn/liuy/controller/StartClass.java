9a8123eb2ed2c29125975fadeae5d16a34aef674
刘洋 <@DESKTOP-C8SDV0L>
2019-12-05 19:36:57 +0800

    Spring JDBC SpringMVC整合的java web项目

---
+++ 01-SpringBoot/src/main/java/cn/liuy/controller/StartClass.java
@@ -0,0 +1,19 @@
+package cn.liuy.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 刘洋
+ * @aim 
+ * SpringBoot启动类(SpringBoot借助启动类将所有对当前项目有用的jar执行)
+*/
+@SpringBootApplication
+public class StartClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(StartClass.class, args);
+	}
+
+}
