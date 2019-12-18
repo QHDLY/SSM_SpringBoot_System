52605d3522911524f37e49291e29f0642d870275
QHDLY <987941243@qq.com>
2019-12-18 11:14:02 +0800

    对书籍进行查询

--- 15-SpringBoot-data-jpa/src/main/java/cn/liuy/dao/UserCrudRespository.java
+++
@@ -1,9 +0,0 @@
-package cn.liuy.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import cn.liuy.pojo.User;
-
-public interface UserCrudRespository extends CrudRepository<User, Integer> {
-
-}
--- 15-SpringBoot-data-jpa/src/main/java/cn/liuy/dao/UserPagingAndSortingRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.liuy.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import cn.liuy.pojo.User;
-
-public interface UserPagingAndSortingRepository extends PagingAndSortingRepository<User, Integer> {
-
-}
